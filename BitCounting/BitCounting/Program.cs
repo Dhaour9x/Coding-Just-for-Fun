2cd0ff0815e2bf29d3a462b250d1f85457f19e1f
Riadh <47638846+Dhaour9x@users.noreply.github.com>
2021-05-18 22:54:08 +0200

    commit

---
+++ BitCounting/BitCounting/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace BitCounting
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CountBits(6);
+        }
+
+
+        public static int CountBits(int n)
+        {
+            var count = n.ToString().Count();
+            count += 1;
+            Console.WriteLine(count);
+            return count;
+        }
+       
+    }
+}
---
+++ BreakCamelCase/BreakCamelCase/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace BreakCamelCase
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(BreakCamelCase("HelloWorld"));
+        }
+
+        private const string Separator = " ";
+        public static List<char> alpha = new() { "A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P",
+                                                    "Q","R","S","T","U","V","W","X","Y","Z" };
+        public static string BreakCamelCase(string str)
+        {
+            for (int i = 0; i < str.Length; i++)
+            {
+                
+                if (str == "")
+                {
+                    return str;
+                }
+                else if (alpha.C str[i].Equals(alpha))
+                {
+                    str.Split(separator: Separator, count: i);
+                    
+                }
+                else
+                {
+                    return str;
+                }
+            }
+            return str;
+        }
+    }
+}
